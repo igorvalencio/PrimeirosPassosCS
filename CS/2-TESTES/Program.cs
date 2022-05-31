9f74a3b8b9d7b03e2064bdf05427e364194e6f48
Igor Neves <igornevesvalencio@hotmail.com>
2022-05-31 19:40:33 -0300

    testes joao

---
+++ CS/2-TESTES/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class programa
+{
+    static void Main(string[] args)
+    {
+        int IdadeJoao = 16;
+        if(IdadeJoao >= 18)
+        {
+            Console.WriteLine("Maior de idade");
+        }
+        else
+        {
+            Console.WriteLine("Menor de idade");
+        }
+    }
+}
