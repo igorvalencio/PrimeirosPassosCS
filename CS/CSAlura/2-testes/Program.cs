e715322b01ba4f7b2d48bf46e044d4d1e016e9ad
Igor Neves <igornevesvalencio@hotmail.com>
2022-05-31 19:11:09 -0300

    testes git e github

---
+++ CS/CSAlura/2-testes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class programa
+{
+    static void Main(string[] args)
+    {
+        int IDADEjoao = 18;
+        if (IDADEjoao >= 18)
+        {
+            Console.WriteLine("Maior de idade");
+        }
+        else
+        {
+            Console.WriteLine("menor de idade");
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
