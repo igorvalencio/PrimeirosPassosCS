cce41137b83c0dea1d0a99e14c3e30cf5ac2e52d
Igor Neves <igornevesvalencio@hotmail.com>
2022-06-01 19:21:07 -0300

    conversoes de variaveis

---
+++ CS/3-variaveisPontoFlutuante/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("ponto flutuante");
+
+        double salario = 8500.52;
+        Console.WriteLine(salario);
+
+        double divisao;
+        divisao = 7.0 / 5;
+        Console.WriteLine(divisao);
+        //importante colocar esse .0 pra ele entender que é um double
+        //se colocar dois numeros inteiros dentro do double, ele devolve um inteiro, ai perde informação
+    }
+}
+
+
---
+++ CS/4-ConversoesEoutrosTipos/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("projeto 4 - conversões");
+
+        double salario = 3000.15;
+        int salarioInteiro = (int)salario;
+        Console.WriteLine(salarioInteiro);
+
+        long x = 1000000000000000000;
+        Console.WriteLine(x);   
+        //long até 64 bits
+
+        short y = 16000;
+        Console.WriteLine(y);
+        //short até 16 bits
+
+        float altura = 1.91f;
+        Console.WriteLine(altura);
+    }
+}
