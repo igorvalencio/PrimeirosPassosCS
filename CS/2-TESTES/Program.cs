8697d00abfd34159002cd04164d50a230ffe2038
Igor Neves <igornevesvalencio@hotmail.com>
2022-06-01 12:32:31 -0300

    tirei umas pastas

--- CS/2-TESTES/Program.cs
+++ CS/2-TESTES/Program.cs
@@ -6,11 +6,11 @@ class programa
         int IdadeJoao = 16;
         if(IdadeJoao >= 18)
         {
-            Console.WriteLine("Maior de idade");
+            Console.WriteLine("Maior de idade!");
         }
         else
         {
-            Console.WriteLine("Menor de idade");
+            Console.WriteLine("Menor de idade!");
         }
     }
 }
