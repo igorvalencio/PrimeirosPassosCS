4849ef8dfd1d69aaf4ea930d3de4417f6c9ebd77
Igor Neves <igornevesvalencio@hotmail.com>
2022-06-01 12:51:18 -0300

    testes

--- CS/2-TESTES/Program.cs
+++ CS/2-TESTES/Program.cs
@@ -3,7 +3,7 @@ class programa
 {
     static void Main(string[] args)
     {
-        int IdadeJoao = 16;
+        int IdadeJoao = 18;
         if(IdadeJoao >= 18)
         {
             Console.WriteLine("Maior de idade!");
