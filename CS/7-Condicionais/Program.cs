a091b67422e64193a3136cc59b6d7b54c2a460bf
Igor Neves <igornevesvalencio@hotmail.com>
2022-06-02 11:18:46 -0300

    treinando if

--- CS/7-Condicionais/Program.cs
+++ CS/7-Condicionais/Program.cs
@@ -4,5 +4,25 @@ class programa
     static void Main(string[] args)
     {
         Console.WriteLine("executando projeto 7 - condicionais");
+
+        int IdadeJoao = 16;
+        int QuantidadePessoas = 3;
+
+        if (IdadeJoao >= 18)
+        {
+            Console.WriteLine("Pode entrar!");
+        }
+        else
+        {
+            if(QuantidadePessoas>=2)
+            {
+                Console.WriteLine("Pode entrar porque está acompanhado");
+            }
+            else
+            {
+                Console.WriteLine("Não pode entrar");
+            }
+            
+        }
     }
 }
