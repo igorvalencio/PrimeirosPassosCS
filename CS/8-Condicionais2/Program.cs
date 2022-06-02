f8233f7fa9b454612643c1e84f6e46d88c4ff60a
Igor Neves <igornevesvalencio@hotmail.com>
2022-06-02 14:38:12 -0300

    condicionais 2

---
+++ CS/8-Condicionais2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("executando projeto 7 - condicionais");
+
+        int IdadeJoao = 18;
+        int QuantidadePessoas = 1;
+
+        bool acompanhado = QuantidadePessoas > 1;
+        bool grupo = true;
+        
+      
+
+        if (IdadeJoao >= 18 || QuantidadePessoas > 1)
+        {
+            Console.WriteLine("Pode entrar!");
+        }
+        else
+        {
+                Console.WriteLine("Não pode entrar");
+        }
+
+        
+    }
+}
