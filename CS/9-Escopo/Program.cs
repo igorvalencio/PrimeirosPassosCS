f0d75fafdff216faada4b7021d0a3d10f91fb206
Igor Neves <igornevesvalencio@hotmail.com>
2022-06-02 15:42:44 -0300

    escopo

---
+++ CS/9-Escopo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+class programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("executando projeto 7 - condicionais");
+
+        int IdadeJoao = 15;
+        int QuantidadePessoas = 2;
+
+        bool acompanhado = QuantidadePessoas > 1;
+     string TextoAdicional;
+
+       if(acompanhado == true)
+        {
+            TextoAdicional = "João está acompanhado";
+        }
+        else
+        {
+            TextoAdicional = "João não está acompanhado";
+        }
+
+        if (IdadeJoao >= 18 || acompanhado)
+        {
+            Console.WriteLine(TextoAdicional);
+            Console.WriteLine("pode entrar");
+        }
+        else
+            Console.WriteLine("Não pode entrar");
+
+      
+
+
+    }
+}
