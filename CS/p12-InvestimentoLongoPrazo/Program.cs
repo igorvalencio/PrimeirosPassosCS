a13508d5061d2bd5171d366c984c684e01b9e22c
Igor Neves <igornevesvalencio@hotmail.com>
2022-06-03 16:57:21 -0300

    encadeando for

---
+++ CS/p12-InvestimentoLongoPrazo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("projeto 12 - Investimento longo prazo");
+
+        double FatorRendimento = 1.005;
+        double investimento = 1000;
+        int mes = 1;
+        
+        for(int anos = 1;  anos <= 5;  anos++)
+        {
+            for ( mes = 1;  mes <= 5;  mes++)
+            {
+                investimento *= FatorRendimento;
+            }
+        } FatorRendimento += 1.005;
+        
+        Console.WriteLine("Depois de 5 anos você terá R$" + investimento);
+    }
+}
