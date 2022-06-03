1571ed93a0f8ce98ee50182c8fd520f2b2db1b58
Igor Neves <igornevesvalencio@hotmail.com>
2022-06-03 14:05:52 -0300

    laço for

---
+++ CS/p11-CalculaPoupanca2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+class programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("executando projeto 11 - simulação de poupança 2");
+
+        //para saber o rendimento de 0,5% ao mês tem que fazer vezes 0.005
+
+
+        /* investimento = investimento + investimento * 0.005;
+         Console.WriteLine(investimento);
+
+         int mes = 1;
+         while (mes <= 12)
+         {
+             investimento = investimento + investimento * 0.005;
+             Console.WriteLine("no mês " + mes + " você tem R$ " + investimento);
+             mes++;
+
+         } */
+
+        double investimento = 1000;
+        for (int mes = 1; mes <= 12; mes++)
+        {
+            investimento *= 1.005;
+            Console.WriteLine("no mês " + mes + " você tem R$ " + investimento);
+            
+        }
+
+    }
+}
