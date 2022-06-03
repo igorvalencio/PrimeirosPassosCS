8f7c84668962a790dbfc439371575ca8314e83fa
Igor Neves <igornevesvalencio@hotmail.com>
2022-06-03 19:11:46 -0300

    ainda encadeando for

---
+++ CS/p13-EncadeandoFor/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+class programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("projeto 13 - encadeando for");
+
+        //com break
+        for (int ContadorLinhas = 0; ContadorLinhas < 10; ContadorLinhas++)
+        {
+            for (int ContadorColuna = 0; ContadorColuna < 10; ContadorColuna++)
+            {
+                Console.Write("*");
+                if (ContadorColuna >= ContadorLinhas)
+                    break;
+
+                
+            }
+            Console.WriteLine();
+        }
+
+        //sem break
+        for(int ContadorLinhas = 0; ContadorLinhas < 10; ContadorLinhas++)
+        {
+            for (int ContadorColuna = 0; ContadorColuna <= ContadorLinhas ; ContadorColuna++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
